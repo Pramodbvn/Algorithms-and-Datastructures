1eda28c46568f5b629a65e7ae083bd119c9c3ea8
Pramodbvn <53940121+Pramodbvn@users.noreply.github.com>
2020-08-02 22:59:53 +0530

    Create SelectionSort.java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class SelectionSort{
+  public static void main(String[] args{
+        int[] arr{10,45,35,2,5,-10,48,65};
+        for(int i=arr.length-1;i>0;i--){
+          int max=0;
+          for(int j=0;j<i+1;j++){
+            if(arr[j]>arr[max]){
+                max=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[max];
+            arr[max]=temp;
+         }
+         System.out.println(Arrays.toString(arr));
+     }
+   }
